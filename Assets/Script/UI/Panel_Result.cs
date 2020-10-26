f8a017701a8bb7d3fc1d345178b2d6578cec3f4d
neive82 <neive@paran.com>
2020-10-26 18:08:20 +0900

    승무패 연출

---
+++ Assets/Script/UI/Panel_Result.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+
+public class Panel_Result : MonoBehaviour
+{
+    public SkeletonGraphic m_Spine_Start;
+    public SkeletonGraphic m_Spine_Fin;
+    public SkeletonGraphic m_Spine_Win;
+    public SkeletonGraphic m_Spine_Draw;
+    public SkeletonGraphic m_Spine_Lose;
+    public GameObject m_Button;
+
+    void Reset()
+    {
+        m_Spine_Start.gameObject.SetActive(false);
+        m_Spine_Fin.gameObject.SetActive(false);
+        m_Spine_Win.gameObject.SetActive(false);
+        m_Spine_Draw.gameObject.SetActive(false);
+        m_Spine_Lose.gameObject.SetActive(false);
+        m_Button.SetActive(false);
+    }
+
+    public void OnStart()
+    {
+        Reset();
+        m_Spine_Start.gameObject.SetActive(true);
+        Invoke("ActiveButton", 2.0f);
+    }
+
+    public void OnFin()
+    {
+        Reset();
+        m_Spine_Fin.gameObject.SetActive(true);
+        Invoke("ActiveButton", 2.0f);
+    }
+
+    public void OnWin()
+    {
+        Reset();
+        m_Spine_Win.gameObject.SetActive(true);
+        Invoke("ActiveButton", 2.0f);
+    }
+
+    public void OnDraw()
+    {
+        Reset();
+        m_Spine_Draw.gameObject.SetActive(true);
+        Invoke("ActiveButton", 2.0f);
+    }
+
+    public void OnLose()
+    {
+        Reset();
+        m_Spine_Lose.gameObject.SetActive(true);
+    }
+
+    public void ActiveButton()
+    {
+        m_Button.SetActive(true);
+    }
+}
