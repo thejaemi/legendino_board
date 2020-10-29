fb01198b3c203699f9629a7540fe4f289162bafa
neive82 <neive@paran.com>
2020-10-29 17:31:19 +0900

    이벤트용 스크립트 누락 추가

--- Assets/Script/Common/Event_Active.cs
+++ Assets/Script/Common/Event_Active.cs
@@ -2,12 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Event_Active : MonoBehaviour
+public class Event_Active : Event_Base
 {
     public GameObject m_Target;
+    /// <summary>
+    /// true = 활성 false = 비활성
+    /// </summary>
     public bool m_Active = true;
+    public float m_Delay = 0.0f;
 
-    public void Run()
+    override public void Run()
+    {
+        if (m_Delay > 0.0f)
+            Invoke("Run_", m_Delay);
+        else
+            Run_();
+    }
+
+    void Run_()
     {
         if (m_Target)
             m_Target.SetActive(m_Active);
