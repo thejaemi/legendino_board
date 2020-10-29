c0a877c7f3d68913019fdd92459ad7a9d3cc76f2
neive82 <neive@paran.com>
2020-10-29 17:30:00 +0900

    Common UI

---
+++ Assets/Script/UI/Panel_Common.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Panel_Common : MonoBehaviour
+{
+    public GameObject m_Dlg_Quit;
+    public GameObject m_Dlg_BattleQuit;
+
+    private void Awake()
+    {
+
+    }
+
+    public void OnBack()
+    {
+        if (SceneManager.GetActiveScene().name == "Lobby" || SceneManager.GetActiveScene().name == "Shop" ||
+            SceneManager.GetActiveScene().name == "Scan")
+            m_Dlg_Quit.SetActive(true);
+
+        if (SceneManager.GetActiveScene().name == "Battle")
+            m_Dlg_BattleQuit.SetActive(true);
+    }
+
+    public void OnBattle_Quit()
+    {
+
+    }
+
+    public void OnBattle_Cancel()
+    {
+
+    }
+
+    public void OnQuit_OK()
+    {
+        Application.Quit();
+    }
+
+    public void OnQuit_Cancel()
+    {
+        m_Dlg_Quit.SetActive(false);
+    }
+}
