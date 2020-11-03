a31bb3c18e4839248ed16f60ab6b4b5155ad3b55
neive82 <neive@paran.com>
2020-11-03 12:11:42 +0900

    스와이프 이펙트 자동 감추기

---
+++ Assets/Script/Common/SimpleEnableAfterDisable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleEnableAfterDisable : MonoBehaviour
+{
+    /// <summary>
+    /// 몇 초 후에 비활성화 할지..
+    /// </summary>
+    public float m_Delay;
+
+    private void OnEnable()
+    {
+        Invoke("Run", m_Delay);
+    }
+
+    void Run()
+    {
+        gameObject.SetActive(false);
+    }
+}
